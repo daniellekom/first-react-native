fd04059c4d6b6a1f02ee8432a48f8495f8d4d0f4
Danielle Kom <daniellekom1@gmail.com>
2022-02-22 14:53:55 -0500

    react native

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button, Alert} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headingText}>Welcome to Walky</Text>
+      <Button onPress={()=>Alert.alert("you pressed the button!")} 
+      title = 'Enter'
+      accessibilityLabel='Press this button to popup an alert!'
+       color="white"  />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headingText: {
+    fontSize: 36,
+    color: 'orange',
+    fontWeight: "900",
+    textAlign: "center",
+  }
 });
---
+++ notes.js
@@ -0,0 +1,2 @@
+// import button add it to stylesheet text etc from reactnative.dev
+//add button to return in app.js and add its features
